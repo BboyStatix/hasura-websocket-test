913f458c9d8d16af35d75cfeb41b49f073be0d2b
Patrick R <patrick.rabier@gmail.com>
2020-11-06 10:27:38 +0800

    ADD: subscriptions

--- src/App.js
+++ src/App.js
@@ -1,60 +1,74 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache, HttpLink } from '@apollo/client';
 import { WebSocketLink } from "@apollo/client/link/ws";
-import { ApolloProvider } from "@apollo/client";
 import Main from "./Main";
+import { setContext } from "apollo-link-context";
+import { getMainDefinition } from "apollo-utilities";
+import { split } from "apollo-link";
 import { SubscriptionClient } from "subscriptions-transport-ws";
-import Main1 from "./Main1";
-import { useState } from "react";
 
-// const HTTP_ENDPOINT = "http://localhost:8080/v1/graphql";
-// const GRAPHQL_ENDPOINT = "ws://localhost:8080/v1/graphql";
-const HTTP_ENDPOINT = "http://hasura-staging.goodcity.hk/v1/graphql";
-const GRAPHQL_ENDPOINT = "wss://hasura-staging.goodcity.hk/v1/graphql";
+const HTTP_ENDPOINT       = "https://hasura-staging.goodcity.hk/v1/graphql";
+const WEBSOCKET_ENDPOINT  = "wss://hasura-staging.goodcity.hk/v1/graphql";
 
-const authToken =
-  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxOSwiYXVkaWVuY2UiOiJnb29kY2l0eSIsImlzc3VlciI6Imdvb2RjaXR5IiwiaHR0cHM6Ly9oYXN1cmEuaW8vand0L2NsYWltcyI6eyJ4LWhhc3VyYS1hbGxvd2VkLXJvbGVzIjpbInN1cGVydmlzb3IiLCJ1c2VyIiwicHVibGljIl0sIngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InN1cGVydmlzb3IiLCJ4LWhhc3VyYS11c2VyLWlkIjoiMTkiLCJ4LWhhc3VyYS1vcmdhbmlzYXRpb24taWRzIjoie30ifSwiaWF0IjoxNjA0NTU2NTYwLCJpc3MiOiJnb29kY2l0eSIsImV4cCI6MTYwNDU2MDE2MCwiaHR0cHM6Ly9nb29kY2l0eS5oay9qd3QvbWV0YWRhdGEiOnsidHlwZSI6ImFwaSJ9fQ.oa1YrK5DV6yAdgwlxtYYHzWJdp3_0bBlnzUOzEzh9pU";
+// const authToken =
+//   "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxOSwiYXVkaWVuY2UiOiJnb29kY2l0eSIsImlzc3VlciI6Imdvb2RjaXR5IiwiaHR0cHM6Ly9oYXN1cmEuaW8vand0L2NsYWltcyI6eyJ4LWhhc3VyYS1hbGxvd2VkLXJvbGVzIjpbInN1cGVydmlzb3IiLCJ1c2VyIiwicHVibGljIl0sIngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InN1cGVydmlzb3IiLCJ4LWhhc3VyYS11c2VyLWlkIjoiMTkiLCJ4LWhhc3VyYS1vcmdhbmlzYXRpb24taWRzIjoie30ifSwiaWF0IjoxNjA0NTU2NTYwLCJpc3MiOiJnb29kY2l0eSIsImV4cCI6MTYwNDU2MDE2MCwiaHR0cHM6Ly9nb29kY2l0eS5oay9qd3QvbWV0YWRhdGEiOnsidHlwZSI6ImFwaSJ9fQ.oa1YrK5DV6yAdgwlxtYYHzWJdp3_0bBlnzUOzEzh9pU";
 
-const httpLink = new HttpLink({
-  uri: HTTP_ENDPOINT,
-  headers: {
-    authorization: authToken,
-  },
-});
+const getHeaders = () => {
+  return {
+    "x-hasura-admin-secret": "ADMIN SECRET"
+  };
+};
 
-const subscriptionClient = new SubscriptionClient(GRAPHQL_ENDPOINT, {
-  reconnect: true,
-  connectionParams: {
-    authToken,
-  },
-});
-const wsLink = new WebSocketLink(subscriptionClient);
-// const wsLink = new WebSocketLink({
-//   uri: GRAPHQL_ENDPOINT,
-//   options: {
-//     reconnect: true,
-//   },
-// });
+const createApolloClient = () => {
+  const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        ...getHeaders()
+      }
+    };
+  });
 
-// const client = new ApolloClient({
-//   // link: httpLink,
-//   link: wsLink,
-//   cache: new InMemoryCache(),
-// });
+  const wsLink = new WebSocketLink(
+    new SubscriptionClient(WEBSOCKET_ENDPOINT, {
+      reconnect: true,
+      timeout: 10000,
+      connectionParams: {
+        headers: getHeaders()
+      }
+    })
+  );
+
+  const httpLink = new HttpLink({
+    uri: HTTP_ENDPOINT,
+    headers: getHeaders()
+  });
+
+  const link = split(
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(query);
+      return kind === "OperationDefinition" && operation === "subscription";
+    },
+    wsLink,
+    httpLink
+  );
+
+  return new ApolloClient({
+    link: authLink.concat(link),
+    cache: new InMemoryCache({
+      addTypename: true
+    })
+  });
+};
 
 function App() {
-  const [showSubscriptions, setShowSubscriptions] = useState(false);
-
-  return null;
-  // return (
-  //   <ApolloProvider client={client}>
-  //     <Main />
-  //     {showSubscriptions && <Main1 />}
-  //     <button onClick={() => setShowSubscriptions(true)}>Subscriptions!</button>
-  //   </ApolloProvider>
-  // );
+  const client = createApolloClient();
+  return (
+    <ApolloProvider client={client}>
+      <Main /> 
+    </ApolloProvider>
+  );
 }
 
 export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,24 +1,47 @@
 import React from "react";
-import { gql, useLazyQuery } from "@apollo/client";
+import { gql, useLazyQuery, useSubscription } from "@apollo/client";
+
 const FETCH_OFFERS = gql`
   query MyQuery {
-    offers {
-      id
+    offers(order_by: {id: asc}) {
+      id,
+      notes
     }
   }
 `;
 
-const Main = () => {
-  const [fetchOffers, { error, data }] = useLazyQuery(FETCH_OFFERS);
+const SUBSCRIBE_TO_OFFERS = gql`
+  subscription MyQuery {
+    offers(order_by: {id: asc}) {
+      id,
+      notes
+    }
+  }
+`;
 
-  console.log({ error, data });
+const style = {
+  borderBottom: "1px solid gray",
+  padding: "20px",
+}
+
+const Main = () => {
+  // const [fetchOffers, { error, data }] = useLazyQuery(FETCH_OFFERS);
+  const { loading, error, data } = useSubscription(SUBSCRIBE_TO_OFFERS);
+
+  if (loading) {
+    return <span>Loading...</span>;
+  }
+  if (error) {
+    console.error(error);
+    return <span>Error!</span>;
+  }
 
   return (
     <div>
-      {data?.offers.map(({ id }) => (
-        <div key={id}>{id}</div>
+      {data?.offers.map(({ id, notes, state }) => (
+        <div style={style} key={id}>[Order {id}] notes="{notes}" state="{state}"</div>
       ))}
-      <button onClick={() => fetchOffers()}>Fetch Offers</button>
+      {/* <button onClick={() => fetchOffers()}>Fetch Offers</button> */}
     </div>
   );
 };
